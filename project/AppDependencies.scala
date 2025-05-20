5ead842dab97184003c1e96e7cc78ecd86680830
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2025-05-20 09:53:13 +0100

    Lined up the dependencies as per request

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,18 +10,18 @@ object AppDependencies {
   private val enumeratumVersion = "1.8.2"
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.0.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"                        % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"                        % "12.0.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                                % hmrcMongoVersion,
     "uk.gov.hmrc"             %% "centralised-authorisation-resource-client-play-30" % "1.5.0",
-    "com.beachape"            %% "enumeratum-play"            % enumeratumVersion,
-    "uk.gov.hmrc"             %% "domain-play-30"             % "11.0.0"
+    "com.beachape"            %% "enumeratum-play"                                   % enumeratumVersion,
+    "uk.gov.hmrc"             %% "domain-play-30"                                    % "11.0.0"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"     % bootstrapVersion            % Test,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"    % hmrcMongoVersion            % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
+    "org.jsoup"               %  "jsoup"                      % "1.13.1"                    % Test,
   )
 
   val it = Seq.empty

43c0901652046975a2beacb914a9722bae6be85c
william-hatzar <22661741+william-hatzar@users.noreply.github.com>
2025-05-23 10:49:09 +0100

    Updated models

---
+++ app/uk/gov/hmrc/ngrpropertylinkingfrontend/models/vmv/VMVResponse.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrpropertylinkingfrontend.models.vmv
+
+import play.api.libs.json.{Format, Json}
+
+case class VMVResponse (total: Int, properties: List[Property])
+
+object VMVResponse {
+
+  implicit val format:Format[VMVResponse] = Json.format[VMVResponse]
+
+}

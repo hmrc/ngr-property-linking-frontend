eeba8f1e15bf6e32a9a524cedb9358dd7092bdd1
Paul <halpo@me.com>
2025-06-18 15:14:44 +0100

    fixed tests

--- test/uk/gov/hmrc/ngrpropertylinkingfrontend/controllers/AddPropertyRequestSentControllerSpec.scala
+++ test/uk/gov/hmrc/ngrpropertylinkingfrontend/controllers/AddPropertyRequestSentControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.ngrpropertylinkingfrontend.controllers
 
 import org.mockito.ArgumentMatchers.any
@@ -25,7 +41,7 @@ class AddPropertyRequestSentControllerSpec extends ControllerSpecSupport with De
   "AddPropertyRequestSentController" must {
 
     "Return OK and the correct view" in {
-      when(mockPropertyLinkingRepo.findByCredId(any())).thenReturn(Future.successful(Some(PropertyLinkingUserAnswers(credId = CredId(null), vmvProperty = testVmvProperty))))
+      when(mockPropertyLinkingRepo.findByCredId(any())).thenReturn(Future.successful(Some(PropertyLinkingUserAnswers(credId = CredId(null), vmvProperty = testVmvProperty, requestSentReference = Some("reference")))))
       val result = controller().show()(authenticatedFakeRequest)
       status(result) mustBe OK
       val content = contentAsString(result)

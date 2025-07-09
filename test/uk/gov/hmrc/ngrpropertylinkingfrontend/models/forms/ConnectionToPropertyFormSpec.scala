13f2294eb483e3ae2f46655517f266a3363077a5
Ashwini Rudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-07-09 09:03:45 +0100

    NGR-2232: add tests for fromString method

--- test/uk/gov/hmrc/ngrpropertylinkingfrontend/models/forms/ConnectionToPropertyFormSpec.scala
+++ test/uk/gov/hmrc/ngrpropertylinkingfrontend/models/forms/ConnectionToPropertyFormSpec.scala
@@ -39,5 +39,15 @@ class ConnectionToPropertyFormSpec extends AnyWordSpec with Matchers {
       boundForm.hasErrors shouldBe true
       boundForm.errors should contain(FormError("connection-to-property-radio", List("connectionToProperty.radio.unselected.error")))
     }
+
+    "return valid ConnectionToPropertyForm to the input string" in {
+      ConnectionToPropertyForm.fromString("Owner") shouldBe ConnectionToPropertyForm.Owner
+      ConnectionToPropertyForm.fromString("Occupier") shouldBe ConnectionToPropertyForm.Occupier
+      ConnectionToPropertyForm.fromString("OwnerAndOccupier") shouldBe ConnectionToPropertyForm.OwnerAndOccupier
+    }
+
+    "throw IllegalArgumentException for invalid ConnectionToPropertyForm input" in {
+      an[IllegalArgumentException] should be thrownBy ConnectionToPropertyForm.fromString("InvalidValue")
+    }
   }
 }
